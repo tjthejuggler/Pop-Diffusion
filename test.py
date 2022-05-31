0e8fa555860c7612cd44d130dccb7a84a492ce3c
TJ <tjthejuggler@gmail.com>
2022-05-31 12:49:40 +0300

    absolute_frame_multiplier should be working

---
+++ test.py
@@ -0,0 +1,27 @@
+import re
+absolute_multiplier = 5
+
+def absolute_key_frame_multiplier(original_string):
+    #if there is no colon in the string, return the original string
+    if not re.search(':', original_string):
+        return original_string
+    #if original_string doesn't include decimals
+    if not re.search('\.', original_string):
+        multiplied_string = re.sub(r'\d+', lambda x: str(int(x.group(0)) * absolute_multiplier), original_string)
+        #print('mult',multiplied_string)
+    #divide all numbers in parentheses by absolute_multiplier
+        return re.sub(r'\((\d+)\)', lambda x: '(' + str(int(x.group(1)) // absolute_multiplier) + ')', multiplied_string)
+    else:
+        #get all non-float numbers in the string
+        non_floats = re.compile("(?<![\.\d])[0-9]+(?![\.\d])")
+        #sub all non-floats with themselves times absolute_multiplier
+        return re.sub(non_floats, lambda x: str(int(x.group(0)) * absolute_multiplier), original_string)
+
+print(absolute_key_frame_multiplier("2"))
+print(absolute_key_frame_multiplier("0.2"))
+print(absolute_key_frame_multiplier("0:(0.0),2:(1.0),12:(0.4)"))
+print(absolute_key_frame_multiplier("0:(10),2:(99),12:(55)"))
+
+
+
+
