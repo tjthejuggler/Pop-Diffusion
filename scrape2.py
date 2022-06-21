9bedf9c4f9bd6f4d4faa45ebffcf4961e729436d
TJ <tjthejuggler@gmail.com>
2022-06-21 14:01:50 +0300

    new auto gist and youtube stuff added to main

--- scrape2.py
+++ scrape2.py
@@ -21,6 +21,9 @@ def main():
     driver.switch_to.active_element
     time.sleep(4)
     html = driver.page_source
+    download_button = driver.find_element_by_xpath("//button[@class='btn btn-info btn-lg' and @data-reactid='.0.1.6.0.2.0.0.1.$64427.2.1.0.0.1.1.1.0.0.0']")
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable(button_element)).click()
+    #<a class="btn btn-info btn-lg" href="http://ccmixter.org/content/airtone/airtone_-_bluenotes_6.mp3" download="" data-reactid=".0.1.6.0.2.0.0.1.$64427.2.1.0.0.1.1.1.0.0.0"><i class="fa fa-cloud-download fa-2x pull-left" data-reactid=".0.1.6.0.2.0.0.1.$64427.2.1.0.0.1.1.1.0.0.0.0"></i><span data-reactid=".0.1.6.0.2.0.0.1.$64427.2.1.0.0.1.1.1.0.0.0.1"> Download </span><small data-reactid=".0.1.6.0.2.0.0.1.$64427.2.1.0.0.1.1.1.0.0.0.2">8.73MB</small></a>
     time.sleep(4)
     print(html)
 
