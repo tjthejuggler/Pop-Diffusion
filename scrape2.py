e2eb85d6f78721ea508a8bd89e2897344ba23420
TJ <tjthejuggler@gmail.com>
2022-06-21 10:02:07 +0300

    scrape and webscrape.ipynb are working to get html from ccmixter search

--- scrape2.py
+++ scrape2.py
@@ -2,24 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 def main():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("window-size=1400,600")
+    driver = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
     driver.get('http://dig.ccmixter.org/search?limit=10&searchp=instrumental')
     html = driver.page_source
     time.sleep(2)
     print(html)
-
-    print('tj')
-
-    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"li.clearfix:nth-child(1) > span:nth-child(3) > button:nth-child(1)"))).click()
-    
-    
-    
+    #use the id number before the song name to get the xpath of the button that is to be clicked
+    button_element = driver.find_element_by_xpath("//button[@class='btn btn-warning  btn-lg' and @data-reactid='.0.1.6.0.2.0.0.1.$64427.2.0']")
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable(button_element)).click()
+    driver.switch_to.active_element
+    time.sleep(4)
     html = driver.page_source
-    time.sleep(10)
+    time.sleep(4)
     print(html)
 
 main()
