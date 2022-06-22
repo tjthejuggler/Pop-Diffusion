6719267f9b6b14f225f03da3c3fb91d0d2a961d3
TJ <tjthejuggler@gmail.com>
2022-06-22 06:39:55 +0300

    get music fix

--- scrape2.py
+++ scrape2.py
@@ -11,12 +11,14 @@ def main():
     options.add_argument("--headless")
     options.add_argument("window-size=1400,600")
     driver = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
-    driver.get('http://dig.ccmixter.org/search?limit=10&searchp=instrumental')
+    driver.get('http://dig.ccmixter.org/search?limit=3715&searchp=instrumental')
     html = driver.page_source
-    time.sleep(2)
+    time.sleep(10)
     print(html)
     #use the id number before the song name to get the xpath of the button that is to be clicked
-    button_element = driver.find_element_by_xpath("//button[@class='btn btn-warning  btn-lg' and @data-reactid='.0.1.6.0.2.0.0.1.$64427.2.0']")
+    
+    button_element = driver.find_element_by_xpath("//button[@class='btn btn-warning  btn-lg' and @data-reactid='.0.1.6.0.2.0.0.1.$60923.2.0']")
+    #button_element = driver.find_element_by_xpath("//button[@class='btn btn-warning  btn-lg' and @data-reactid='.0.1.6.0.2.0.0.1.$64427.2.0']")
     WebDriverWait(driver,10).until(EC.element_to_be_clickable(button_element)).click()
     driver.switch_to.active_element
     time.sleep(4)
