ce52d015e60d5bd81936fbacf7d8f527f96950c7
TJ <tjthejuggler@gmail.com>
2022-06-26 13:06:21 +0300

    remove and reorganized files

--- dynamic_prompt_generators.py
+++ dynamic_prompt_generators.py
@@ -105,17 +105,17 @@ image_prompts = {
 people_artists = ['Edward Burne-Jones', 'Karol Bak', 'Jeff Easley', 'Brian Froud', 'Wendy Froud', 'Peter Gric', 'Jim Burns', 'Michael Whelan', 'Tyler Edlin', 'H.P. Lovecraft', 'Tim Burton', 'Yoshitaka Amano', 'Wes Anderson', 'Esao Andrews', 'Giuseppe Arcimboldo', 'Banksy', 'Ivan Bilibin', 'Alexander Bogen', 'Sandro Botticelli', 'Caravaggio', 'Camille Corot', 'Walter Crane', 'Geof Darrow', 'Eugene Delacroix', 'Paul Delvaux', 'Edmund Dulac', 'Albrecht Dürer', 'Larry Elmore', 'John Philip Falter', 'Caspar David Friedrich', 'Francisco Goya', 'Rebecca Guay', 'Winslow Homer', 'Edward Hopper', 'Frida Kahlo', 'Gustav Klimt', 'Ilya Kuvshinov', 'Rene Magritte', 'Don Maitz', 'John Martin', 'Peter Max', 'Robert McCall', 'Ralph McQuarrie', 'Frank Miller', 'Moebius', 'Chris Moore', 'Alphonse Mucha', 'Ted Nasmith', 'Victo Ngai', 'Rafal Olbinski', 'Diego Rivera', 'Hubert Robert', 'Andreas Rocha', 'Norman Rockwell', 'Egon Schiele', 'Ivan Shishkin', 'Marc Simonetti', 'Zack Snyder', 'Joaquín Sorolla', 'Simon Stalenhag', 'Ross Tran', 'Vincent Van Gogh', 'Rembrandt van Rijn', 'Ron Walotsky', 'Andy Warhol', 'Liam Wong', 'Yuumei', 'Amir Zand', 'Edgar Degas', 'Asher Brown Durand', 'Giovanni Battista Gaulli', 'Donato Giancola', 'John Atkinson Grimshaw', 'James Gurney', 'Jamie Hewlett', 'Ernst Ludwig Kirchner', 'Claude Monet', 'Igor Morski', 'Victor Moscoso', 'Craig Mullins', 'Nicholas Roerich', 'Hans Baldung', 'John Bauer', 'Julie Bell', 'Hans Bellmer', 'Mary Blair', 'William Blake', 'Victor Brauner', 'Gaston Bussière', 'Claude Cahun', 'Clyde Caldwell', 'Canaletto', 'Marcel Chagall', 'James C. Christensen', 'Giorgio de Chirico', 'Evelyn De Morgan', 'Henri de Toulouse-Lautrec', 'Gustave Doré', 'Jesper Ejsing', 'Harold Elliott', 'Anton Otto Fischer', 'Art Fitzpatrick', 'Jean-Honoré Fragonard', 'Victoria Francés', 'Frank Frazetta', 'Josan Gonzalez', 'Bastien Lecouffe-Deharme', 'George Luks', 'Victor Adame Minguez', 'Peter Mohrbacher', 'Ilya Repin', 'RHADS', 'Masamune Shirow', 'Coby Whitmore', 'Alex Grey', 'Junji Ito', 'Mati Klarwein', 'Kazimir Malevich', 'Clovis Trouille', 'Diego Velázquez', 'Qian Xuan', 'Nele Zirnite', 'Simon Bisley', 'Pascal Blanché', 'Arnold Böcklin', 'Camille-Pierre Pambu Bodo', 'William-Adolphe Bouguereau', 'Eleanor Vere Boyle', 'Arik Brauer', 'Pieter Bruegel the Elder', 'Paul Cézanne', 'Leonardo da Vinci', 'Richard Dadd', 'Max Ernst', 'Kelly Freas', 'Ernst Fuchs', 'Henry Fuseli', 'Paul Gauguin', 'Emma Geary', 'Artemisia Gentileschi', 'Warwick Goble', 'Rob Gonsalves', 'Jane Graverol', 'Henriette Grindat', 'Matthias Grünewald', 'Thomas Häfner', 'Sydney Prior Hall', 'Keith Haring', 'Rudolf Hausner', 'Brothers Hildebrandt', 'Tim Hildebrandt', 'Greg Hildebrandt', 'William Hogarth', 'Kati Horna', 'Valentine Hugo', 'George Inness', 'Jarosław Jaśnikowski', 'Joe Jusko', 'Filippino Lippi', 'Bob Ringwood', 'Jakub Rozalski', 'Greg Rutkowski', 'Mike Winkelmann', 'Seb McKinnon', 'Daniel Merriam', 'Sylvain Sarrailh', 'Tom Lovell', 'Haddon Sundblom', 'Eddie Mendoza', 'Eugeniusz Zak', 'Frank Zappa', 'Pablo Dominguez', 'Cicely Mary Barker', 'Amy Brown', 'Stephanie Law', 'Neil Welliver', 'Child Hassam', 'Henri Matisse', 'Jules Dupré', 'Pieter Brueghel the Elder', 'Theodore Robinson', 'Alexei Savrasov', 'Arkhip Kuindzhi', 'Claude Lorraine', 'Pablo Amaringo', 'Pascal Blanche', 'Adam Varga', 'Android Jones', 'Simon Hennessey', 'Denis Peterson', 'Robert Bechtle', 'Gustavo Silva Nuez', 'Claudio Bravo', 'Gottfried Helnwein', 'István Sándorfi', 'Nestor Leynes', 'Franz Gertsch', 'Kareem Olamilekan', 'Dragan Malešević Tapi', 'Ian Hornak', 'Otto Duecker', 'Antoni Taulé', 'Willem van Veldhuizen', 'Andrey Lekarski', 'Walther Jervolino', 'Diego Koi', 'Allyssa Monks', 'Luiz Escanuela', 'Dirk Dzimirsky']
 import random
 random_artist = random.choice(people_artists)
-    text_prompts = {}
-    frames_scale_input = ""
-    frame_skip_steps_input = ""
+text_prompts = {}
+frames_scale_input = ""
+frame_skip_steps_input = ""
 
-    for i in range(110):
+for i in range(110):
     # text_prompts[i] = ["Beautiful detailed portrait of a man from the year "+str(((i+1)*25)+100)+" by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation:4",
     #    "text:-2", "glasses:-1"]
     j = i
     if j > 10:
         text_prompts[i] = [
-            "Beautiful detailed portrait of a "+str(j-15)+" year old teddy bear by " +random.choice(people_artists)+", Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:4",
+            "Beautiful detailed portrait of a "+str(j-10)+" year old teddy bear by " +random.choice(people_artists)+", Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:4",
             "text:-2",
             "glasses:-1"
         ]
@@ -218,6 +218,194 @@ for i in range(110):
           "glasses:-1",
           "color: 1"
       ]
+
+image_prompts = {
+    # 0:['ImagePromptsWorkButArentVeryGood.png:2',],
+}
+
+ #AGING WITH WEIGHT MORPH ------------------------------
+
+text_prompts = {}
+x_weight = -10
+y_weight = 10
+for i in range(120):
+    if i > 10:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of a "+str(i-10)+" year old tree person hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "evil: "+str(x_weight),
+            "adorable: "+str(y_weight)
+        ]
+    elif i > 5:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of a "+str(i-5)+" month old tree person hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "evil: "+str(x_weight),
+            "adorable: "+str(y_weight)
+        ]
+    else:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of an adorable "+str(i)+" day old baby tree person hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "evil: "+str(x_weight),
+            "adorable: "+str(y_weight),
+            "color: 3"
+        ]
+    if i % 6 == 0:
+        x_weight+=1
+    if (i+3) % 6 == 0:
+        y_weight-=1
+
+image_prompts = {
+    # 0:['ImagePromptsWorkButArentVeryGood.png:2',],
+}
+
+#FULL EVOLUTION LIST-----------------------------------------------------
+
+"planet earth",
+"water",
+"single-celled organism",
+"bacteria",
+"viruses",
+"land fungi",
+"clams",
+"shrimp",
+"octopus",
+"jellyfish",
+"scorpions",
+"spiders",
+"housefly",
+"earthworms",
+"lizards",
+"crabs",
+"sharks",
+"snakes",
+"birds",
+"beetles",
+"dinosaurs",
+"frogs",
+"bees",
+"crocodiles",
+"ants",
+"rodents",
+"penguins",
+"owls",
+"parrots",
+"bats",
+"camels",
+"butterflies",
+"bears",
+"pigs",
+"cats",
+"pelicans",
+"deer",
+"ostriches",
+"giraffes",
+"platypuses",
+"Mammoths",
+"blue whales",
+"swordfish",
+"coyotes",
+"orangutan",
+
+#NATIONALITIES MORPH------------------------------------------------------
+nationalities = ["Afghan","Algerian","Angolan","Argentine","Austrian","Australian","Bangladeshi","Belarusian","Belgian","Bolivian","Bosnian/Herzegovinian","Brazilian","British","Bulgarian","Cambodian","Cameroonian","Canadian","Central African","Chadian","Chinese","Colombian","Costa Rican","Croatian","Czech","Congolese","Danish","Ecuadorian","Egyptian","Salvadoran","English","Estonian","Ethiopian","Finnish","French","German","Ghanaian","Greek","Guatemalan","Dutch","Honduran","Hungarian","Icelandic","Indian","Indonesian","Iranian","Iraqi","Irish","Israeli","Italian","Ivorian","Jamaican","Japanese","Jordanian","Kazakh","Kenyan","Lao","Latvian","Libyan","Lithuanian","Malagasy","Malaysian","Malian","Mauritanian","Mexican","Moroccan","Namibian","New Zealand","Nicaraguan","Nigerien","Nigerian","Norwegian","Omani","Pakistani","Panamanian","Paraguayan","Peruvian","Philippine","Polish","Portuguese","Congolese","Romanian","Russian","Saudi, Saudi Arabian","Scottish","Senegalese","Serbian","Singaporean","Slovak","Somalian","South African","Spanish","Sudanese","Swedish","Swiss","Syrian","Thai","Tunisian","Turkish","Turkmen","Ukranian","Emirati","American","Uruguayan","Vietnamese","Welsh","Zambian","Zimbabwea"]
+
+frames_skip_steps_input = ""
+text_prompts = {}
+for frame in range(len(nationalities) * 4):
+    age = frame//4
+    nationality = nationalities[age]
+    type = 'senior'
+    if age < 5:
+        type = 'baby'
+    elif age < 12:
+        type = 'child'
+    elif age < 19:
+        type = 'teenager'
+    elif age < 55:
+        type = 'adult'
+    elif age < 65:
+        type = 'elderly person'
+    
+    text_prompts[frame] = ["Beautiful detailed portrait of a "+str(age)+" day old "+nationality+" "+type+" by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation:4",
+    "text:-2", "glasses:-1", "color:1" ]
+    #frames_skip_steps_input = "1: (85), 50: (65), 100: (85)" #@param{type: 'string'}
+    if frame > 0:
+        if frame%4 == 0:
+            frames_skip_steps_input += str(frame)+":(40),"
+        elif frame%4 == 1:
+            frames_skip_steps_input += str(frame)+":(60),"
+        elif frame%4 == 3:
+            frames_skip_steps_input += str(frame)+":(80),"
+
+frames_skip_steps_input = frames_skip_steps_input[:-1]
+frames_skip_steps = absolute_key_frame_multiplier(frames_skip_steps_input)
+
+#EXTENDED AGING WITH WEIGHT MORPH---------------------------------
+
+text_prompts = {}
+creepy_weight = -10
+ador_weight = 12
+for i in range(180):
+    if i > 160:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of a "+str(i-160)+" year old haunted demon bones by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "creepy: "+str(creepy_weight),
+        ]  
+    if i > 130:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of a "+str(i-130)+" year old tree skeleton hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "creepy: "+str(creepy_weight),
+        ]      
+    elif i > 30:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of a "+str(i-30)+" year old tree person hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "creepy: "+str(creepy_weight),
+            "adorable: "+str(ador_weight)
+        ]
+    elif i > 25:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of a "+str(i-25)+" month old tree person hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+             "adorable: "+str(ador_weight)
+        ]
+    elif i > 20:
+        text_prompts[i] = [
+            "Beautiful detailed portrait of an adorable "+str(i-20)+" day old baby tree person hybrid by Nick Silva, Shin JeongHo, Wandah Kurniawan, Symmetrical composition with people centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "adorable: "+str(ador_weight),
+            "color: 3"
+        ]
+    elif i > 10:
+          text_prompts[i] = [
+            "Beautiful detailed picture of a "+str(i-10)+" year old tree by Nick Silva, Shin JeongHo, Jim Burns, Symmetrical composition with tree centered, trending on artstation, colorized:12",
+            "text:-6",
+            "glasses:-3",
+            "color: 3"
+        ]
+    else:
+          text_prompts[i] = [
+            "Beautiful detailed picture of a "+str(i)+" day old plant seedling by Nick Silva, Shin JeongHo, Jim Burns, Symmetrical composition with plant centered, trending on artstation, colorized:12",
+            "text:-6",
+            "color: 3"
+        ]
+    
+    if i>30 and (i+5) % 10 == 0:
+          creepy_weight+=1
+    if i>20 and i % 10 == 0:
+          ador_weight-=1
 
 image_prompts = {
     # 0:['ImagePromptsWorkButArentVeryGood.png:2',],
